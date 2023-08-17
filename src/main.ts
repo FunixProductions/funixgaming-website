c19f8fbb80f59f8864afc51a0abaa5cce4934ee3
FunixG <antoine.pronnier@gmail.com>
2023-08-17 10:36:38 +0200

    added bootstrap to the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+/// <reference types="@angular/localize" />
 
-import { AppModule } from './app/app.module';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
