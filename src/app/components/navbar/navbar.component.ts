b60f27ea47a5a128594352fc4a443d64427135c8
FunixG <antoine.pronnier@gmail.com>
2023-08-17 14:56:28 +0200

    navbar v1 done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {faDiscord, faInstagram, faTiktok, faTwitch, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {faX, IconDefinition} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  twitchLogo: IconDefinition = faTwitch;
+  youtubeLogo: IconDefinition = faYoutube;
+  twitterLogo: IconDefinition = faX;
+  instagramLogo: IconDefinition = faInstagram;
+  discordLogo: IconDefinition = faDiscord;
+  tiktokLogo: IconDefinition = faTiktok;
 
 }
